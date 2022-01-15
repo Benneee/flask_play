0d65c19b93c9b54df55c11eef054bc9920af36e0
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-15 05:27:08 +0100

    Feat: added form validation and post method handling

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
 
 
