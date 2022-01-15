91f9e88c1e0337d4085ba4de26c8b41fc2c5655e
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-15 02:02:37 +0100

    Feat: using the render_template method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,8 +7,8 @@ app.config['DEBUG'] = True
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html')
