0f5a20aa00ce11447b49e6e1e4501807ef6f107c
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2022-01-16 03:32:01 +0100

    Feat: reset password and email sending

--- flaskplay/__init__.py
+++ flaskplay/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -12,6 +14,14 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # Has to be the function name of our route to the login page
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.mail.yahoo.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+
+mail = Mail(app)
+
 
 # Imported here to avoid circular import error since app is also imported in the routes file
 from flaskplay import routes
